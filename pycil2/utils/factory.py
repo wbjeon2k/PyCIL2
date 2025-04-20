4f7d40bd5343cbd1ec52fe3f028f0fc7ff45a5d3
wbjeon2k <wbjeon2k@users.noreply.github.com>
2025-04-20 01:38:33 +0000

    modified as packages, update cfg.py

--- pycil2/utils/factory.py
+++ pycil2/utils/factory.py
@@ -77,3 +77,84 @@ def get_model(model_name, args):
         return TagFex(args)
     else:
         assert 0
+
+
+def get_strategy(strategy_name, args):
+    name = strategy_name.lower()
+    if name == "icarl":
+        from pycil2.strategy.icarl import iCaRL
+        return iCaRL(args)
+    elif name == "bic":
+        from pycil2.strategy.bic import BiC
+        return BiC(args)
+    elif name == "podnet":
+        from pycil2.strategy.podnet import PODNet
+        return PODNet(args)
+    elif name == "lwf":
+        from pycil2.strategy.lwf import LwF
+        return LwF(args)
+    elif name == "ewc":
+        from pycil2.strategy.ewc import EWC
+        return EWC(args)
+    elif name == "wa":
+        from pycil2.strategy.wa import WA
+        return WA(args)
+    elif name == "der":
+        from pycil2.strategy.der import DER
+        return DER(args)
+    elif name == "finetune":
+        from pycil2.strategy.finetune import Finetune
+        return Finetune(args)
+    elif name == "replay":
+        from pycil2.strategy.replay import Replay
+        return Replay(args)
+    elif name == "gem":
+        from pycil2.strategy.gem import GEM
+        return GEM(args)
+    elif name == "coil":
+        from pycil2.strategy.coil import COIL
+        return COIL(args)
+    elif name == "foster":
+        from pycil2.strategy.foster import FOSTER
+        return FOSTER(args)
+    elif name == "rmm-icarl":
+        from pycil2.strategy.rmm import RMM_FOSTER, RMM_iCaRL
+        return RMM_iCaRL(args)
+    elif name == "rmm-foster":
+        from pycil2.strategy.rmm import RMM_FOSTER, RMM_iCaRL
+        return RMM_FOSTER(args)
+    elif name == "fetril":
+        from pycil2.strategy.fetril import FeTrIL 
+        return FeTrIL(args)
+    elif name == "pass":
+        from pycil2.strategy.pa2s import PASS
+        return PASS(args)
+    elif name == "il2a":
+        from pycil2.strategy.il2a import IL2A
+        return IL2A(args)
+    elif name == "ssre":
+        from pycil2.strategy.ssre import SSRE
+        return SSRE(args)
+    elif name == "memo":   
+        from pycil2.strategy.memo import MEMO
+        return MEMO(args)
+    elif name == "beefiso":
+        from pycil2.strategy.beef_iso import BEEFISO
+        return BEEFISO(args)
+    elif name == "simplecil":
+        from pycil2.strategy.simplecil import SimpleCIL
+        return SimpleCIL(args)
+    elif name == "acil":
+        from pycil2.strategy.acil import ACIL
+        return ACIL(args)
+    elif name == "ds-al":
+        from pycil2.strategy.dsal import DSAL
+        return DSAL(args)
+    elif name == "aper_finetune":
+        from pycil2.strategy.aper_finetune import APER_FINETUNE
+        return APER_FINETUNE(args)
+    elif name == "tagfex":
+        from pycil2.strategy.tagfex import TagFex
+        return TagFex(args)
+    else:
+        assert 0
